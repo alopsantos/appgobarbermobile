46953aa5a3493f1a61c6ed854eae6de18d4b0b7f
Anderson Lopes dos Santos <lopscorp@iMac-de-Anderson.local>
2020-11-04 00:48:54 -0300

    Cadastro completo

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,15 @@
-import React, {useEffect, useRef} from 'react';
-import {TextInputProps} from 'react-native';
-import {useField} from '@unform/core';
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+  useCallback
+} from 'react';
+import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
-import {Container, TextInput, Icon} from './styles';
+import { Container, TextInput, Icon } from './styles';
 
 interface InputProps extends TextInputProps {
   name: string;
@@ -12,12 +19,37 @@ interface InputProps extends TextInputProps {
 interface InputValueReference {
   value: string;
 }
-const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
+
+interface InputRef {
+  focus(): void;
+}
+
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
+  { name, icon, ...rest },
+  ref,
+) => {
   const inputElementRef = useRef<any>(null);
 
-  const {registerField, defaultValue = '', fieldName, error} = useField(name);
-  const inputValueRef = useRef<InputValueReference>({value: ''});
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const inputValueRef = useRef<InputValueReference>({ value: '' });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+  const handlerInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
   useEffect(() => {
     registerField<string>({
       name: fieldName,
@@ -25,7 +57,7 @@ const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
       path: 'value',
       setValue(ref: any, value) {
         inputValueRef.current.value = value;
-        inputElementRef.current.setNativeProps({text: value});
+        inputElementRef.current.setNativeProps({ text: value });
       },
       clearValue() {
         inputValueRef.current.value = '';
@@ -34,20 +66,23 @@ const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
     });
   }, [fieldName, registerField]);
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused} isErrored={!!error}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
 
       <TextInput
         ref={inputElementRef}
+        onFocus={handleInputFocus}
+        onBlur={handlerInputBlur}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
-        onChangeText={(value) => {
+        onChangeText={value => {
           inputValueRef.current.value = value;
         }}
-        {...rest}></TextInput>
+        {...rest}
+      ></TextInput>
     </Container>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,20 +1,25 @@
-import React, {useRef} from 'react';
-import {Form} from '@unform/mobile';
+import React, { useRef, useCallback } from 'react';
+import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
 import {
   Image,
   View,
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  TextInput,
+  Alert
 } from 'react-native';
 
+import api from '../../services/api';
+import getValidationErrors from '../../utils/getValidationErros';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import logoImg from '../../assets/logo.png';
+import logoImg from '../../assets/logo.png'; //eslin ignore
 
 import {
   Container,
@@ -23,16 +28,69 @@ import {
   BackToSignInButtonTex,
 } from './styles';
 
+interface SignInFormData{
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
   const navigation = useNavigation();
+
+  const handleSignUp = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório!'),
+        email: Yup.string()
+          .required('E-mail obrigatorio')
+          .email('Digite um e-email válido'),
+        password: Yup.string().required('Sua senha é obrigatorio'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await api.post('/users', data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você ja pode fazer login na aplicação.'
+      )
+
+      navigation.goBack();
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        return;
+      }
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao fazer cadastro, tente novamente.',
+      );
+    }
+  }, [navigation]);
+
   return (
     <>
       <KeyboardAvoidingView
-        style={{flex: 1}}
+        style={{ flex: 1 }}
         enabled
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{flex: 1}}>
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
           <Container>
             <Image source={logoImg} />
 
@@ -40,18 +98,45 @@ const SignUn: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Form ref={formRef}
-              onSubmit={(data) => {
-                console.log(data);
-              }}>
-              <Input name="name" icon="user" placeholder="Nome" />
-              <Input name="email" icon="mail" placeholder="E-mail" />
-              <Input name="password" icon="lock" placeholder="Senha" />
+            <Form
+              ref={formRef}
+              onSubmit={handleSignUp}
+            >
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {emailInputRef.current?.focus();}}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                returnKeyType="next"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                onSubmitEditing={() => {passwordInputRef.current?.focus();}}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="send"
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
 
               <Button
                 onPress={() => {
-                  formRef.current?.submitForm()
-                }}>
+                  formRef.current?.submitForm();
+                }}
+              >
                 Entrar
               </Button>
             </Form>
